9d9d4f4d5161f8a09b1ecf9bb375c981208cd638
johnsontool <sduanjucc@gmail.com>
2025-10-23 14:07:12 +0800

    update github url and site-url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,14 +6,14 @@ const siteMetadata = {
   description: 'Work smarter with the right tools.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://johnstool.net',
+  siteRepo: 'https://github.com/johnsontool/myblog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   //mastodon: 'https://mastodon.social/@mastodonuser',
 
   email: 'contact@johnstool.net',
-  github: 'https://github.com',
+  github: 'https://github.com/johnsontool/myblog',
   //x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
   //facebook: 'https://facebook.com',
