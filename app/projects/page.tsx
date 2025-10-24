8c790fd0b76a67b53f1c9a69236846fc1c699934
johnsontool <sduanjucc@gmail.com>
2025-10-24 14:14:13 +0800

    deploy vercel

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,10 @@ export default function Projects() {
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Projects
           </h1>
-          <p style={{ display: 'none' }} className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          <p
+            style={{ display: 'none' }}
+            className="text-lg leading-7 text-gray-500 dark:text-gray-400"
+          >
             Showcase your projects with a hero image (16 x 9)
           </p>
         </div>
